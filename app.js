bbb2302ec78b6ac9ea915bb7cbed2add3cb227a4
Derek <derekdanielson84@gmail.com>
2023-08-08 10:23:31 -0600

    Added classList toggle

--- app.js
+++ app.js
@@ -6,3 +6,14 @@
 
 const navToggle = document.querySelector('.nav-toggle');
 const links = document.querySelector('.links');
+
+
+navToggle.addEventListener('click', function(){
+    //if (links.classList.contains('show-link')){
+    //    links.classList.remove('show-links');
+    //} else {
+    //    links.classList.add('show-links');
+    //}
+    
+    links.classList.toggle('show-links');
+});
